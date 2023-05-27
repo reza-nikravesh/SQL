0c53b46155d8f4cd2231dcaa2706283f2c93bee9
Reza Nikravesh <reza.nikravesh80@gmail.com>
2023-05-27 11:23:21 +0330

    create customer and order tbls

--- main.sql
+++ main.sql
@@ -82,7 +82,7 @@ CREATE TABLE Product(
     name VARCHAR(25),
     category_name VARCHAR(25),
     photo VARCHAR(250) NOT NULL,
-    vendor_id VARCHAR(25) NOT NULL,
+    vendor_id INT NOT NULL,
     price NUMBER,
     description VARCHAR(250),
     PRIMARY KEY(id)
@@ -93,3 +93,47 @@ ADD FOREIGN KEY(category_name) REFERENCES Category(name);
 -- ************************************************
 ALTER TABLE Product
 ADD FOREIGN KEY(vendor_id) REFERENCES Vendor(id);
+-- ************************************************
+CREATE TABLE Customer(
+    id AUTOINCREMENT NOT NULL UNIQUE,
+    first_name VARCHAR(25),
+    last_name VARCHAR(25),
+    province_name VARCHAR(25) NOT NULL,
+    city_name VARCHAR(25) NOT NULL,
+    street VARCHAR(25),
+    house_number NUMBER,
+    email VARCHAR(25),
+    PRIMARY KEY(id)
+);
+-- ************************************************
+CREATE TABLE Order(
+    id AUTOINCREMENT NOT NULL UNIQUE,
+    customer_id VARCHAR(25),
+    product_id VARCHAR(25),
+    order_number NUMBER NOT NULL,
+    delivery_date DATE,
+    province_name VARCHAR(25) NOT NULL,
+    city_name VARCHAR(25) NOT NULL,
+    street VARCHAR(25),
+    house_number NUMBER,
+    PRIMARY KEY(id)
+);
+-- ************************************************
+ALTER TABLE Customer
+ADD FOREIGN KEY(province_name) REFERENCES Province(name);
+-- ************************************************
+ALTER TABLE Customer
+ADD FOREIGN KEY(city_name) REFERENCES City(name);
+-- ************************************************
+ALTER TABLE Order
+ADD FOREIGN KEY(province_name) REFERENCES Province(name);
+-- ************************************************
+ALTER TABLE Order
+ADD FOREIGN KEY(city_name) REFERENCES City(name);
+-- ************************************************
+ALTER TABLE Order
+ADD FOREIGN KEY(product_id) REFERENCES Customer(id);
+-- ************************************************
+ALTER TABLE Order
+ADD FOREIGN KEY(customer_id) REFERENCES Product(id);
+-- ************************************************
