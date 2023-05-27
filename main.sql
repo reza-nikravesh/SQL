8dd54f5c23f566518984414e62c25cdbb89dd2c8
Reza Nikravesh <reza.nikravesh80@gmail.com>
2023-05-27 10:26:08 +0330

    add vendor table

--- main.sql
+++ main.sql
@@ -1,11 +1,31 @@
+-- ************************************************
 CREATE TABLE City(
     name VARCHAR(25) NOT NULL,
     province_name VARCHAR(25) NOT NULL,
     PRIMARY KEY(name)
 );
+-- ************************************************
 CREATE TABLE Province(
     name VARCHAR(25) NOT NULL UNIQUE,
     PRIMARY KEY(name)
 );
+-- ************************************************
 ALTER TABLE City
 ADD FOREIGN KEY(province_name) REFERENCES Province(name);
+-- ************************************************
+CREATE TABLE Vendor(
+    id INT AUTO_INCREMENT NOT NULL UNIQUE,
+    first_name VARCHAR(25),
+    last_name VARCHAR(25),
+    province_name VARCHAR(25) NOT NULL,
+    city_name VARCHAR(25) NOT NULL,
+    email VARCHAR(25),
+    phone VARCHAR(25),
+    PRIMARY KEY(id)
+);
+-- ************************************************
+ALTER TABLE Vendor
+ADD FOREIGN KEY(province_name) REFERENCES Province(name);
+-- ************************************************
+ALTER TABLE Vendor
+ADD FOREIGN KEY(city_name) REFERENCES City(name);
