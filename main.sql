dd53ce9646129a4bcd5791ab533f5eb304e90b00
Reza Nikravesh <reza.nikravesh80@gmail.com>
2023-05-27 10:48:18 +0330

    create job position , organization, employee tables

--- main.sql
+++ main.sql
@@ -29,3 +29,44 @@ ADD FOREIGN KEY(province_name) REFERENCES Province(name);
 -- ************************************************
 ALTER TABLE Vendor
 ADD FOREIGN KEY(city_name) REFERENCES City(name);
+-- ************************************************
+CREATE TABLE JobPosition(
+    name VARCHAR(25) NOT NULL UNIQUE,
+    description VARCHAR(250),
+    PRIMARY KEY(name)
+);
+-- ************************************************
+CREATE TABLE Organization(
+    name VARCHAR(25) NOT NULL UNIQUE,
+    description VARCHAR(250),
+    manager_id VARCHAR(25),
+    PRIMARY KEY(name)
+);
+-- ************************************************
+CREATE TABLE Employee(
+    id AUTOINCREMENT NOT NULL UNIQUE,
+    first_name VARCHAR(25),
+    last_name VARCHAR(25),
+    province_name VARCHAR(25) NOT NULL,
+    city_name VARCHAR(25) NOT NULL,
+    salary NUMBER,
+    email VARCHAR(25),
+    job_position_name VARCHAR(25),
+    organization_name VARCHAR(25),
+    PRIMARY KEY(id)
+);
+-- ************************************************
+ALTER TABLE Employee
+ADD FOREIGN KEY(province_name) REFERENCES Province(name);
+-- ************************************************
+ALTER TABLE Employee
+ADD FOREIGN KEY(city_name) REFERENCES City(name);
+-- ************************************************
+ALTER TABLE Employee
+ADD FOREIGN KEY(job_position_name) REFERENCES JobPosition(name);
+-- ************************************************
+ALTER TABLE Employee
+ADD FOREIGN KEY(organization_name) REFERENCES Organization(name);
+-- ************************************************
+ALTER TABLE Organization
+ADD FOREIGN KEY(manager_id) REFERENCES Employee(id)
