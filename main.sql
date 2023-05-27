e41eeee7fbb749d8401649474434869aa47384ec
Reza Nikravesh <reza.nikravesh80@gmail.com>
2023-05-27 10:15:27 +0330

    add city and province tables

--- main.sql
+++ main.sql
@@ -1,3 +1,11 @@
-create table Province(province_name VARCHAR(25),
-
-)
+CREATE TABLE City(
+    name VARCHAR(25) NOT NULL,
+    province_name VARCHAR(25) NOT NULL,
+    PRIMARY KEY(name)
+);
+CREATE TABLE Province(
+    name VARCHAR(25) NOT NULL UNIQUE,
+    PRIMARY KEY(name)
+);
+ALTER TABLE City
+ADD FOREIGN KEY(province_name) REFERENCES Province(name);
